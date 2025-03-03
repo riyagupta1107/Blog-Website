31fe73e4fab1b1306141ff8d4fc2eb704cfd949b
riyagupta1107 <riya.gupta070705@gmail.com>
2025-03-04 02:15:17 +0530

    04.03.35

--- Starter/assets/js/main.js
+++ Starter/assets/js/main.js
@@ -1,13 +1,50 @@
 // Grab elements
 
+// selector is the parameter
+const selectElement = selector => {
+    const element = document.querySelector(selector)
+    if (element) return element;
+    throw new Error(`Something went wrong, make sure the ${selector} exists!`);
+};
+
+//console.log(selectElement('.navbar'));
+
 //Nav styles on scroll
+const scrollHeader = () => {
+    const headerElement = selectElement('#header');
+    if (this.scrollY >= 15) {
+        headerElement.classList.add('activated');
+    } else {
+        headerElement.classList.remove('activated');
+    }
+};
 
+window.addEventListener('scroll', scrollHeader);
 // Open menu & search pop-up
+const menuToggleIcon = selectElement('#menu-toggle-icon');
 
+const toggleMenu = () => {
+    const mobileMenu = selectElement('#menu');
+    mobileMenu.classList.toggle('activated');
+    menuToggleIcon.classList.toggle('activated');
+};
+
+menuToggleIcon.addEventListener('click', toggleMenu);
 // Open/Close search form popup
 
 // -- Close the search form popup on ESC keypress
 
 // Switch theme/add to local storage
+const bodyElement = document.body;
+const themeToggleBtn = selectElement('#theme-toggle-btn');
 
+themeToggleBtn.addEventListener('click',() => {
+    bodyElement.classList.toggle('light-theme');
+
+    if (bodyElement.classList.contains('light-theme')) {
+        localStorage.setItem('currentTheme', 'themeActive'); //key-value pair
+    } else {
+        localStorage.setItem('currentTheme', 'themeActive');
+    }
+});
 // Swiper
